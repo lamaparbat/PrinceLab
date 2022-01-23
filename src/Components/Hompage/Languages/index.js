a34feba3d894b9602f8c004fa166d5249bde1553
Parbat Lama <lamaparbat70@gmail.com>
2022-01-23 23:51:58 +0545

    Languages component completed.

---
+++ src/Components/Hompage/Languages/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../Languages/index.css';
+
+function index() {
+ return (
+  <>
+   <div className='container languages'>
+    <center>
+     <h1 id='title'>Languages we ought to simplify</h1>
+    </center><br />
+    <div className='icon_container'>
+     <img
+      src={process.env.PUBLIC_URL + "/assets/python.png"}
+      className='img-fluid' id='icon' />
+     <img
+      src={process.env.PUBLIC_URL + "/assets/tensorflow.png"}
+      className='img-fluid' id='icon' />
+     <img
+      src={process.env.PUBLIC_URL + "/assets/flutter.png"}
+      className='img-fluid' id='icon' />
+     <img
+      src={process.env.PUBLIC_URL + "/assets/arduino.png"}
+      className='img-fluid' id='icon' />
+    </div>
+   </div>
+  </>
+);
+}
+
+export default index;
--- src/Components/Hompage/index.js
+++ src/Components/Hompage/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../Hompage/index.css';
+import Languages from '../Hompage/Languages/index';
 
 function index() {
  return(
@@ -10,8 +11,10 @@ function index() {
       className='video w-100'
       src={process.env.PUBLIC_URL + "/assets/banner.mp4"}
       autoPlay loop muted></video>
-    </div>
-   </div>
+       </div>
+     </div><br/><br/>
+     
+     <Languages />
  </>);
 }
 
