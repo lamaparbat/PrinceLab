7080d26a37ad6b22a7114be705d542849b9737a0
Parbat Lama <lamaparbat70@gmail.com>
2022-01-30 23:44:52 +0545

    Erron on longitude and latitude while fetching from  geolocation

--- src/App.js
+++ src/App.js
@@ -10,32 +10,38 @@ function App() {
   // current user location data
   const [longitude, setLongitude] = useState("");
   const [latitude, setLatitude] = useState("");
-  const [userLocation, setUserLocation] = useState("")
+  const [userLocation, setUserLocation] = useState({
+    country: "",
+    place: "",
+    temperature: "",
+    weather: "",
+    time_status:""
+  })
   const [timeStatus, setTime] = useState("");
-  
+
   // convert the timestamp to local time
   const convertTimeFormat = (timestamp) => {
     // store timestamp to string var
     const string = timestamp.toLocaleTimeString();
-    
+
     // split the hrs & status(am||pm) only
     const hrs = string.substr(0, 2);
     const status = string.substr(string.length - 2, string.length);
-    
+
     //determine day or night
     if (hrs >= 6 && status == "PM" || hrs <= 12 && hrs >= 6 && status == "AM") {
       setTime("night")
     } else {
       setTime("day")
     }
-    
+
     //update the time status
-    setTime(hrs+""+status)
+    setTime(hrs + "" + status)
   }
-  
+
   // weathermap api key
   const key = "e9d1627bffbc7cc590db1097ac22326c";
-  
+
   // tract the user geolocation
   useEffect(() => {
     //tract the current user location 
@@ -45,29 +51,31 @@ function App() {
       setLongitude(position.coords.longitude);
       setLatitude(position.coords.latitude);
     });
-    
+
     // fetch the location using axios
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`)
-        .then(res => {
-          setUserLocation({
-            country: res.data.sys.country,
-            place: res.data.name,
-            temperature: res.data.main.temp + "degree celcius",
-            weather: res.data.weather[0].description,
-            time_status: timeStatus
-          })
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`)
+      .then(res => {
+        setUserLocation({
+          country: res.data.sys.country,
+          place: res.data.name,
+          temperature: res.data.main.temp + "degree celcius",
+          weather: res.data.weather[0].description,
+          time_status: timeStatus
         })
+        console.log(res.data)
+      })
     
-  },[]);
-  
+    console.log(userLocation)
+  }, []);
+
   return (
-    <div className="App">    
+    <div className="App">
       {/* navbar */}
       <Navbar />
-      
+
       {/* video banner */}
       <Homepage />
-      
+
       {/* Footer */}
       <Footer />
     </div>
