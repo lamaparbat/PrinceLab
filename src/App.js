93bda0a34a1f148cc0ddf1f07cf058c9db27de11
Parbat Lama <lamaparbat70@gmail.com>
2022-01-30 23:38:50 +0545

    Track user location using Navigator browser object & Weathermap api to tract the place

--- src/App.js
+++ src/App.js
@@ -1,12 +1,67 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Homepage from './Components/Hompage/index';
 import Navbar from '../src/Components/Navbar/Index';
 import Footer from '../src/Components/Footer/index';
+import axios from 'axios';
 
 function App() {
+  // current user location data
+  const [longitude, setLongitude] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [userLocation, setUserLocation] = useState("")
+  const [timeStatus, setTime] = useState("");
+  
+  // convert the timestamp to local time
+  const convertTimeFormat = (timestamp) => {
+    // store timestamp to string var
+    const string = timestamp.toLocaleTimeString();
+    
+    // split the hrs & status(am||pm) only
+    const hrs = string.substr(0, 2);
+    const status = string.substr(string.length - 2, string.length);
+    
+    //determine day or night
+    if (hrs >= 6 && status == "PM" || hrs <= 12 && hrs >= 6 && status == "AM") {
+      setTime("night")
+    } else {
+      setTime("day")
+    }
+    
+    //update the time status
+    setTime(hrs+""+status)
+  }
+  
+  // weathermap api key
+  const key = "e9d1627bffbc7cc590db1097ac22326c";
+  
+  // tract the user geolocation
+  useEffect(() => {
+    //tract the current user location 
+    navigator.geolocation.getCurrentPosition(position => {
+      const timestamp = new Date(position.timestamp);
+      convertTimeFormat(timestamp);
+      setLongitude(position.coords.longitude);
+      setLatitude(position.coords.latitude);
+    });
+    
+    // fetch the location using axios
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`)
+        .then(res => {
+          setUserLocation({
+            country: res.data.sys.country,
+            place: res.data.name,
+            temperature: res.data.main.temp + "degree celcius",
+            weather: res.data.weather[0].description,
+            time_status: timeStatus
+          })
+        })
+    
+  },[]);
+  
   return (
-    <div className="App">
+    <div className="App">    
       {/* navbar */}
       <Navbar />
       
