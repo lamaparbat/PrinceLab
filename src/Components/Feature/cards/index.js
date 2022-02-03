8c23c2a41e29b05e6a71f159266b5d0f3cbf7e20
Parbat Lama <lamaparbat70@gmail.com>
2022-02-03 23:37:12 +0545

    card box component completed. Responsive testing remaining...

--- src/Components/Feature/cards/index.js
+++ src/Components/Feature/cards/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import '../cards/index.css';
 
+// card1
+const Card1 = () => {
+ return (
+  <>
+   <div className='card1'>
+    <img
+     src={process.env.PUBLIC_URL + "/assets/rpl.png"}
+    />
+    <h5 className='my-4'>Create / Rename / Delete</h5>
+    <button className='px-5 py-2'>Scripts</button>
+   </div>
+ </>)
+}
+
+// card1
+const Card2 = () => {
+ return (
+  <>
+   <div className='card2'>
+    <img
+     src={process.env.PUBLIC_URL + "/assets/python.png"}
+    />
+    <h5 className='mt-5'>Python Function Inbuilt</h5>
+   </div>
+  </>)
+}
 
 function index() {
   return (
-    <div className='cards bg-danger p-3'>
-      asd
+    <div className='container cards_container py-4 my-2'>
+    <Card1 />
+    <Card2 />
     </div>
   )
 }
