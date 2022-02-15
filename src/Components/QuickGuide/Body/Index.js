3ad9c17841e8b0c0292a9c49968f35adc6088df9
Parbat Lama <lamaparbat70@gmail.com>
2022-02-15 20:35:21 +0545

    changes

---
+++ src/Components/QuickGuide/Body/Index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//main function
+const Index = () => {
+    return(
+        <div className="quick_guide">
+           <Sidebar />
+            <Body />
+        </div>
+    )
+}
