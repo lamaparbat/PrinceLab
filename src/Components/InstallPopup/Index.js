bf205f3c13ab260e442b82f5894a18159d94b5ff
Parbat Lama <lamaparbat70@gmail.com>
2022-02-24 21:45:20 +0545

    InstallPop component navbar completed

--- src/Components/InstallPopup/Index.js
+++ src/Components/InstallPopup/Index.js
@@ -5,26 +5,31 @@ import {CancelOutlined} from "@mui/icons-material";
 const Index = () => {
     const Navbar = () => {
         return(
-            <div className="install_navbar">
-               <div className={"brand_cont"}>
-                   <div className={"install_navbar_img"}>
-                       <img src={process.env.PUBLIC_URL+"/assets/plane.png"} />
-                   </div>
-                   <div className="brand">
-                       <h3>Discover music</h3>
-                       <span>Image classification for diseases</span><br />
-                       <h6>Princelab</h6>
-                       <span className={"text-primary"}>More info</span>
-                   </div>
-               </div>
-                <CancelOutlined />
+            <div className="install_navbar px-3">
+              <div className={"install_navbar_row"}>
+                  <div className={"brand_cont"}>
+                      <div className={"install_navbar_img"}>
+                          <img src={process.env.PUBLIC_URL+"/assets/cloud.png"} />
+                      </div>
+                      <div className="brand py-1">
+                          <h5>Discover music</h5>
+                          <span>Image classification for diseases</span><br />
+                          <h6>Princelab</h6>
+                          <span className={"text-primary btn p-0"}>More info</span>
+                      </div>
+                  </div>
+                  <CancelOutlined className={"text-secondary btn p-0"} />
+              </div>
             </div>
         )
     }
 
     return(
-        <div className="">
+        <div className="discover">
             <Navbar />
+            <div className={"discover_content"}>
+
+            </div>
         </div>
     )
 }
