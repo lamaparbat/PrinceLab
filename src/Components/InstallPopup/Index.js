4301a43f25dd712f1298dbbc5326cf12e9255b33
Parbat Lama <lamaparbat70@gmail.com>
2022-02-25 06:59:02 +0545

    Discover install popup model dark mode maintained ...

--- src/Components/InstallPopup/Index.js
+++ src/Components/InstallPopup/Index.js
@@ -11,7 +11,7 @@ const InputComp = () => {
         <div className="input_div">
             <h5>Recorded list</h5>
             <div className="input_div_item mt-3">
-                <AudioFileOutlinedIcon className="p-2" />
+                <AudioFileOutlinedIcon className="p-2 icons" />
                 <span>Audio_File1.png</span><br />
             </div><br/><br />
             <button className="btn btn-primary w-100 rounded-2">Predict</button>
@@ -25,7 +25,7 @@ const OutputComp = () => {
         <div className="input_div p-0">
             <h5 className={"bg-primary text-white p-2 px-3"} id="result_title">Result</h5>
             <div className="input_div_item mt-3 w-75 mx-4">
-                <AudioFileOutlinedIcon className="p-2" />
+                <AudioFileOutlinedIcon className="p-2 icons" />
                 <span>Audio_File1.png</span><br />
             </div><br/><br />
         </div>
@@ -37,10 +37,10 @@ const  InstallPopupBottomNavbar = () => {
     return(
         <div className="installBottomNav mb-5 w-100">
            <div className={"rounded-pill btn mx-4 save p-2"}>
-               <SaveAltRoundedIcon className={"p-1"} />
+               <SaveAltRoundedIcon className={"p-1 icons"} />
            </div>
             <div className={"rounded-pill btn mic p-2"}>
-                <MicRoundedIcon className={"p-1"} />
+                <MicRoundedIcon className={"p-1 icons"} />
             </div>
         </div>
     )
