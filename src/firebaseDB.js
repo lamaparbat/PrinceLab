37293576413c3eaad811a39bcd40fbc87f4c0782
Parbat Lama <lamaparbat70@gmail.com>
2022-03-01 12:01:21 +0545

    Code refined .....

--- src/firebaseDB.js
+++ src/firebaseDB.js
@@ -1,25 +1,3 @@
-// import firebase from 'firebase/compat/app';
-// import 'firebase/compat/auth';
-// import 'firebase/compat/database';
-//
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBaFJovrxpB34C2Rpj4Kt9fpDsZKpyO1eM",
-//     authDomain: "princelab-b263f.firebaseapp.com",
-//     databaseURL: "https://princelab-b263f-default-rtdb.firebaseio.com",
-//     projectId: "princelab-b263f",
-//     storageBucket: "princelab-b263f.appspot.com",
-//     messagingSenderId: "395718624144",
-//     appId: "1:395718624144:web:06fc2dc31ad9cb203fa915"
-// };
-//
-// // Initialize Firebase
-// const app = firebase.initializeApp(firebaseConfig);
-//
-// // Use these for db & auth
-// const db = app.database();
-// const auth = app.auth();
-//
-// export { app, auth, db };
 
 // Import the functions you need from the SDKs you need
 import firebase from 'firebase/compat/app';
@@ -27,7 +5,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB-n4q7JJ1gkV7FjwK59Nyy5OyH-gjPFSQ",
+    apiKey: process.env.DB_API_KEY,
     authDomain: "princelab-f13cd.firebaseapp.com",
     databaseURL: process.env.DB_URL,
     projectId: "princelab-f13cd",
