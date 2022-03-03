27ab645948fca4b56d61562a95e77d119708a15a
Parbat Lama <lamaparbat70@gmail.com>
2022-03-03 22:39:27 +0545

    Remove .env scret key

--- src/firebaseDB.js
+++ src/firebaseDB.js
@@ -5,9 +5,18 @@ import 'firebase/compat/auth';
 import 'firebase/compat/database';
 
 const firebaseConfig = {
-    apiKey:`${process.env.REACT_APP_DB_API_KEY}`,
-    authDomain: "https://lamaparbat.github.io/prince",
-    databaseURL: process.env.REACT_APP_DB_URL,
+    // apiKey:`${process.env.REACT_APP_DB_API_KEY}`,
+    // authDomain: "princelab-f13cd.firebaseapp.com",
+    // databaseURL: process.env.REACT_APP_DB_URL,
+    // projectId: "princelab-f13cd",
+    // storageBucket: "princelab-f13cd.appspot.com",
+    // messagingSenderId: "539468354501",
+    // appId: "1:539468354501:web:ed4643c0d2dc19ec3631ac",
+    // measurementId: "G-FTKN9B48MW"
+
+    apiKey: "AIzaSyB-n4q7JJ1gkV7FjwK59Nyy5OyH-gjPFSQ",
+    authDomain: "princelab-f13cd.firebaseapp.com",
+    databaseURL: "https://princelab-f13cd-default-rtdb.firebaseio.com",
     projectId: "princelab-f13cd",
     storageBucket: "princelab-f13cd.appspot.com",
     messagingSenderId: "539468354501",
