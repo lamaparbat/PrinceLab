09425702ed150ad52b56400776f9724f1c41e1c1
Parbat Lama <lamaparbat70@gmail.com>
2022-05-12 19:21:23 +0545

    .ENV file replace by custom function

---
+++ src/Components/AiModelWidget/SoundRecognition/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ImageClassification from '../ImageClassification/index';
+
+function Index() {
+ return (
+  <ImageClassification id="sound_recog" />
+ )
+}
+
+export default Index;
--- src/secret.js
+++ src/secret.js
@@ -1,3 +1,5 @@
+
+
 export const Secret = () => {
  return "iamparbat9817890667";
 }
