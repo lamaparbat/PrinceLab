87336ad4c045d9ee0b269bbcbb15c517929c79e7
Parbat Lama <lamaparbat70@gmail.com>
2022-06-24 20:27:13 +0545

    seo testing

--- src/Components/Hompage/AboutEnvironment/index.js
+++ src/Components/Hompage/AboutEnvironment/index.js
@@ -13,11 +13,11 @@ function index() {
         <div className='content'>
           <h1><b>Simplest <span>Environment</span></b></h1>
           <h3>With the most tools and function</h3>
-          <p className='my-3'>Sketch is the home for your entire collaborative design
+          {/* <p className='my-3'>Sketch is the home for your entire collaborative design
             process. From early ideas to pixel-perfect artwork,
             playable prototypes and developer handoff. It all starts
             here. This will result in one ACMI installment plan over 12
-            months for the eligible iPad or Mac</p>
+            months for the eligible iPad or Mac</p> */}
           <font className={"text-white font-weight-bold btn p-0"}>Learn how to use it <ArrowRightAltIcon
               className={"text-dark"}
               id="rightArrowIcon"
