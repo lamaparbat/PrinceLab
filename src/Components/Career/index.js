709c4567a6ce26defeef5810f44d42732c938245
Parbat Lama <lamaparbat70@gmail.com>
2022-06-24 16:37:52 +0545

    SEO TESTING WITH HELMET ON OTHER PAGE

--- src/Components/Career/index.js
+++ src/Components/Career/index.js
@@ -1,4 +1,5 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
+import Helmet from 'react-helmet';
 import '../Career/index.css';
 import Front from '../Career/Front/index.js';
 import Beta from '../Career/Beta/index.js';
@@ -14,6 +15,16 @@ function Index() {
 
     return (
         <div className='container-fluid text-center p-0 career'>
+            <Helmet>
+                <title>Princelab / Career</title>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name='description'
+                    content='Career provide by princelab. How to get a job in princelab company as software developer, web developer, flutter developer, ios developer, react developer, python developer django developer'
+                    key="desc" />
+                <meta name='keywords' content='Career, Job, Software developer, Flutter, React, IOS, Android, Web, ML, Django, Python' />
+                <meta name="author" content="Prince kumar singh" />
+            </Helmet>
             <Front/>
             <Beta/>
             <Journey/><br/><br/>
